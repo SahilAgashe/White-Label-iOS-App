1431339770d84f5c3680d90850fba64ca7fd5049
Sahil Agashe <sahil@scrambleapps.gmail.com>
2024-02-14 18:09:48 +0530

    Initial Commit

---
+++ White-Label-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  White-Label-Demo
+//
+//  Created by SAHIL AMRUT AGASHE on 14/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
