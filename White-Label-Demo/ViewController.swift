48ae4b6585155bda029f6dbb8813b2e12720af0f
Sahil Agashe <sahil@scrambleapps.gmail.com>
2024-02-14 19:10:44 +0530

    Duplicated current target so , app has two targets now. Renamed both targets to Yellow and Cyan. Added display name and bundle-identifier for each target. Shown Labels on UI displaying app name and version.

--- White-Label-Demo/ViewController.swift
+++ White-Label-Demo/ViewController.swift
@@ -11,7 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+        
+        let appName = Bundle.main.infoDictionary?["CFBundleName"] as? String ?? ""
+        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+        print("appName => \(appName) , appVersion => \(appVersion)")
+        
+        let appNameLabel = UILabel()
+        appNameLabel.text = "App Name: \(appName)"
+    
+        let appVersionLabel = UILabel()
+        appVersionLabel.text = "App Version: \(appVersion)"
+        
+        let vstack = UIStackView(arrangedSubviews: [appNameLabel, appVersionLabel])
+        vstack.axis = .vertical
+        vstack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(vstack)
+        
+        NSLayoutConstraint.activate([
+            vstack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            vstack.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 
